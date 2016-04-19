9969c584e161d0749f7d2602529449a7f14faa96
jgmorrison <jgmorrison84@yahoo.com>
2016-04-19 13:12:27 -0500

    First commit for fake_maker

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "fake_maker",
+    version = "0.0.1",
+    author = "John Morrison",
+    author_email = "jgmorrison84@gmail.com",
+    description = ("A tool for quickly generating a CSV of user data."),
+    packages = ["fake_maker", "tests"]
+)
