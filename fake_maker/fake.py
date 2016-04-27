1462295180928630de0e26467d69ae7ff2db3b3e
jgmorrison <jgmorrison84@yahoo.com>
2016-04-27 12:03:15 -0500

    Corrected fake.py for PEP8 guidelines

--- fake_maker/fake.py
+++ fake_maker/fake.py
@@ -1,14 +1,14 @@
 from faker import Faker
 import random
 
+
 class FakeMake(object):
 
-    def __init__(self): 
+    def __init__(self):
         self.fake = Faker()
-        
 
     def _gender(self):
-        x = random.randint(1,2)
+        x = random.randint(1, 2)
         if x == 1:
             return "Male"
         elif x == 2:
@@ -16,26 +16,30 @@ class FakeMake(object):
 
     def _first_name(self, gender):
         if gender == "Male":
-        	return self.fake.first_name_male()
+            return self.fake.first_name_male()
         elif gender == "Female":
-        	return self.fake.first_name_female()
+            return self.fake.first_name_female()
 
     def personal_data(self, amount):
         file = open('fakemaker.csv', 'w')
-        file.write('First_Name,Last_Name,Address,City,State,Zip,Joined,Age,Gender,Email\n')
+        file.write('First_Name,Last_Name,Address,City,State,Zip,Joined,\
+Age,Gender,Email\n')
 
         for run in range(amount):
             gender = self._gender()
-            data = [self._first_name(gender), self.fake.last_name(), self.fake.street_address(), \
-            self.fake.city(), self.fake.state_abbr(), self.fake.postalcode(), self.fake.date(), \
-            str(random.randint(18, 90)), gender, self.fake.email(), "\n"]
-            
+            data = [self._first_name(gender), self.fake.last_name(),
+                    self.fake.street_address(), self.fake.city(),
+                    self.fake.state_abbr(), self.fake.postalcode(),
+                    self.fake.date(), str(random.randint(18, 90)), gender,
+                    self.fake.email(), "\n"]
+
             for item in data:
                 if item == data[-2] or item == data[-1]:
                     file.write(item)
                 else:
                     file.write(item + ",")
-            
+
         file.close()
 
-    #call the make function so that it is automatic on instantiation with amount.
+#    call the make function so that it is automatic on instantiation /
+#    with amount.
