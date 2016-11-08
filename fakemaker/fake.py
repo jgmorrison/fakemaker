840c414aa9231cbefbd443e960ba3d9e8909c401
jgmorrison <jmorrison@continuum.io>
2016-11-08 13:32:40 -0600

    Removed old fake.py and renamed newfake.py to fake.py.

--- fakemaker/fake.py
+++ fakemaker/fake.py
@@ -2,10 +2,19 @@ from random import randint
 from faker import Faker
 
 
-class FakeMake(object):
+class Customer(object):
 
     def __init__(self):
         self.fake = Faker()
+        self.gender = self._gender()
+        self.first_name = self._first_name(self.gender)
+        self.last_name = self.fake.last_name() 
+        self.age = randint(18, 90)
+        self.street_address = self.fake.street_address()
+        self.state = self.fake.state_abbr()
+        self.zip_code = randint(10000, 99500)
+        self.date = self.fake.date()
+        self.email = self.fake.email()
 
     def _gender(self):
         selection = randint(1, 2)
@@ -20,26 +29,14 @@ class FakeMake(object):
         elif gender == "Female":
             return self.fake.first_name_female()
 
-    def personal_data(self, amount):
-        fake_file = open('fakemaker.csv', 'w')
-        fake_file.write('First_Name,Last_Name,Address,City,State,Zip,Joined,\
-Age,Gender,Email\n')
+    def __repr__(self):
+        return "{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}".format(
+		self.last_name, self.first_name, self.age, self.gender, 
+		self.street_address, self.state, self.zip_code, 
+		self.date, self.email)
 
-        for i in range(amount):
-            gender = self._gender()
-            data = [self._first_name(gender), self.fake.last_name(),
-                    self.fake.street_address(), self.fake.city(),
-                    self.fake.state_abbr(), str(randint(10000, 99500)),
-                    self.fake.date(), str(randint(18, 90)), gender,
-                    self.fake.email(), "\n"]
 
-            for item in data:
-                if item == data[-2] or item == data[-1]:
-                    fake_file.write(item)
-                else:
-                    fake_file.write(item + ",")
-
-        fake_file.close()
-
-#    call the make function so that it is automatic on instantiation /
-#    with amount.
+def printer(amount):
+    for i in range(amount):
+        customer = Customer()
+        print(customer)
